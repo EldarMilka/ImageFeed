cda63242272b31259fb8b7be0093b91ee090edaf
Eldar <eldar200207@mail.ru>
2025-05-05 17:14:04 +0300

    final

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,11 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Эльдар on 04.05.2025.
+//
+
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
