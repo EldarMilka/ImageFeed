b26b266e6a23819f0a636042615ba3b77d3b2619
Eldar <eldar200207@mail.ru>
2025-05-09 16:38:37 +0300

    final3

---
+++ ImagesList/ImagesListCell.swift
@@ -0,0 +1,19 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Эльдар on 04.05.2025.
+//
+
+import UIKit
+
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    
+    @IBOutlet weak var CellImage: UIImageView!
+    
+    @IBOutlet weak var DateLabel: UILabel!
+    @IBOutlet weak var LikeButtom: UIButton!
+}
